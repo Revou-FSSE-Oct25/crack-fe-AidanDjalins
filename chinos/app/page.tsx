807563850a832acf64c5037ac59ec49a72860920
AidanDjalins <aidan.djalins@binus.ac.id>
2026-05-07 10:43:32 +0700

    Added locomotive scrolling for smooth scrolling experience

--- chinos/app/page.tsx
+++ chinos/app/page.tsx
@@ -1,4 +1,7 @@
-import Image from "next/image";
+"use client"
+
+import { useEffect } from "react";
+
 import HeroBanner from "./components/HeroBanner";
 import Explore from "./components/Explore";
 import About from "./components/About";
@@ -6,6 +9,15 @@ import Locations from "./components/Locations";
 
 
 export default function HomePage() {
+
+  useEffect(() => {
+      const loadLocomotiveScroll = async () => {
+      const LocomotiveScroll = (await import("locomotive-scroll")).default;
+      new LocomotiveScroll();
+    }
+
+    loadLocomotiveScroll();
+  }, []);
   return (
     <div className="h-full overflow-x-hidden">
       <HeroBanner />
